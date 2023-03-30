ce2bfc977003119b8b0c00e19f100679f0ed2ee1
Muhammed Mustafa Alnaddaf <alnaddafmustafa3@gamil.com>
2023-03-30 22:36:23 +0300

    add solution

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0, 2);
+};
+
+console.log(returnFirstTwoDrivers(drivers)); 
+
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+
+
+const fareDoubler = createFareMultiplier(2);
+
+console.log(fareDoubler(10)); 
+
+const selectDifferentDrivers = function(drivers, selectingFunction) {
+    return selectingFunction(drivers);
+  };
+
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers)); 
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers)); 
+
